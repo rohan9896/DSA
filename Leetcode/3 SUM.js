50abdc2c746709eced93afdfa4fccc934df35afa
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2024-11-29 18:50:25 +0530

    Update 3 SUM.js

--- Leetcode/3 SUM.js
+++ Leetcode/3 SUM.js
@@ -1,20 +1,21 @@
 export function findSumOfThree(nums, target) {
     
-    const sortedArr = nums.sort();
+    const sortedArr = nums.sort((a, b) => a-b);
     
     
-    for(let i=0;i<sortedArr.length + 2;i++) {
+    for(let i=0;i<sortedArr.length - 2;i++) {
+      
       const currentElement = sortedArr[i];
       
       let leftPointerIdx = i+1;
-      let rightPointerIdx = sortedArr.length-1;
+      let rightPointerIdx = sortedArr.length -1;
       
       while(leftPointerIdx < rightPointerIdx) {
-        if(currentElement + sortedArr[leftPointerIdx] + sortedArr[rightPointerIdx] === target) {
+        if(sortedArr[leftPointerIdx] + sortedArr[rightPointerIdx] === target - currentElement) {
           return true;
         }
         
-        if(currentElement + sortedArr[leftPointerIdx] + sortedArr[rightPointerIdx] < target) {
+        if(sortedArr[leftPointerIdx] + sortedArr[rightPointerIdx] < target - currentElement) {
           leftPointerIdx++;
         } else {
           rightPointerIdx--;
