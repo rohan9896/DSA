4d6c39f8db7250b988d72aa8f31e9dc8751f594a
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2024-12-07 21:53:15 +0530

    Create SinglyLinkedList.js

---
+++ Data Structures Implementation/SinglyLinkedList.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
