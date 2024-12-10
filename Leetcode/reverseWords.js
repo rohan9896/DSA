80738eb50b33d8e534511e7458bdfba78c48f8e1
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2024-12-10 14:59:26 +0530

    Update reverseWords.js

--- Leetcode/reverseWords.js
+++ Leetcode/reverseWords.js
@@ -21,7 +21,42 @@ function reverseWordsUsingStack(sentence) {
     return reversedSentence.trim();
 }
 
-
+// Reverse words using 2 pointers
+function reverseWords(sentence) {
+  let reversedSentenceArray = sentence.trim().replace(/\s+/g, " ").split('').reverse().filter(item => Boolean(item));
+  
+  let leftPointer = 0;
+  let rightPointer = 0;
+  
+  while(rightPointer <= reversedSentenceArray.length - 1) {
+    
+    while(rightPointer < reversedSentenceArray.length && reversedSentenceArray[rightPointer] !== ' ') {
+      rightPointer++;
+    }
+    
+    let start = leftPointer;
+    let end = rightPointer - 1;
+    
+    while(start <= end) {
+      const temp = reversedSentenceArray[start];
+      reversedSentenceArray[start] = reversedSentenceArray[end];
+      reversedSentenceArray[end] = temp;
+      
+      start++
+      end--;
+      
+    }
+    
+    // Now set leftPointer and rightPointer to next non-space alphabet
+    while(reversedSentenceArray[rightPointer] === ' ') {
+      rightPointer++;
+    }
+    
+    leftPointer = rightPointer;
+  }
+  
+  return reversedSentenceArray.join(''); 
+}
 
 export {
     reverseWordsUsingStack
